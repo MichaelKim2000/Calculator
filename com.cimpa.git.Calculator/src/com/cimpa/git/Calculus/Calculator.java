42156532b270ac508d43e393499a8b60cf82feaf
MichaelKim2000 <103054850+MichaelKim2000@users.noreply.github.com>
2022-04-14 12:27:21 +0200

    added div #789

--- com.cimpa.git.Calculator/src/com/cimpa/git/Calculus/Calculator.java
+++ com.cimpa.git.Calculator/src/com/cimpa/git/Calculus/Calculator.java
@@ -7,9 +7,12 @@ public class Calculator {
 		
 		Addition addNumbers = new Addition();
 		Substraction subNumbers = new Substraction();
+		Division divNumbers = new Division();
+		
 		
 		System.out.println("2+2 is: " + addNumbers.add(2,2));
 		System.out.println("2-2 is: " + subNumbers.sub(2,2));
+		System.out.println("2/2 is: " + divNumbers.div(2,2));
 
 	}
 
