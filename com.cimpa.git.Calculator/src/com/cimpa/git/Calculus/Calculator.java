51bed32b772f2f2589643613fe2a9564277b7bd3
MichaelKim2000 <103054850+MichaelKim2000@users.noreply.github.com>
2022-04-14 08:35:32 +0200

    added readme added addition

--- com.cimpa.git.Calculator/src/com/cimpa/git/Calculus/Calculator.java
+++ com.cimpa.git.Calculator/src/com/cimpa/git/Calculus/Calculator.java
@@ -4,6 +4,10 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Calculator! 2");
+		
+		Addition addNumbers = new Addition();
+		
+		System.out.println("2+2 is: " + addNumbers.add(2,2));
 
 	}
 
