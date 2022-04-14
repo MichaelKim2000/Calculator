3f8e7864acc3a237a6bb8d820f52a2bcf7521cb0
MichaelKim2000 <103054850+MichaelKim2000@users.noreply.github.com>
2022-04-14 08:35:22 +0200

    udpated system message

--- com.cimpa.git.Calculator/src/com/cimpa/git/Calculus/Calculator.java
+++ com.cimpa.git.Calculator/src/com/cimpa/git/Calculus/Calculator.java
@@ -3,7 +3,7 @@ package com.cimpa.git.Calculus;
 public class Calculator {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Calculator!");
+		System.out.println("Hello Calculator! 2");
 
 	}
 
