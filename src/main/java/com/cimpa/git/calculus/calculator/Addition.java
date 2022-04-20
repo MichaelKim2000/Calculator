d54731f407173ff77c12e6ed683579a1754d2b50
MichaelKim2000 <103054850+MichaelKim2000@users.noreply.github.com>
2022-04-20 13:14:01 +0200

    added javadoc

--- src/main/java/com/cimpa/git/calculus/calculator/Addition.java
+++ src/main/java/com/cimpa/git/calculus/calculator/Addition.java
@@ -1,5 +1,11 @@
 package com.cimpa.git.calculus.calculator;
 
+/**
+ * Baseline: adding two Integers with no modifications
+ * @author mkim
+ *
+ */
+
 public class Addition extends Calculate {
 	@Override
 	public int action(int x, int y) {
